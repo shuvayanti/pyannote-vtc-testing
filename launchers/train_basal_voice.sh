76f3704b96e4bb51c52b0dc65e88ed6da659d111
hadware <hadwarez@gmail.com>
2022-01-21 02:14:04 +0100

    created train script for basal voice

---
+++ launchers/train_basal_voice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodelist=puck5
+#SBTACH --gres=gpu:rtx8000:1
+
+# loading modules and activating the right conda env
+source ~htiteux/.bashrc
+module load cuda/10.0 anaconda
+conda activate pyannote-vtc-v2
+
+python main.py runs/basal_voice/ train \
+-p BasalVoice.SpeakerDiarization.InterviewDiarizationProtocol
+--classes basalvoice
+--model_type pyannet
+--epoch 200
