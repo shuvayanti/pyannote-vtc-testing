377e2db9f16a2c82a9ef0dd349a7309b8f77d2e0
shuvayanti <shuvayanti@gmail.com>
2023-08-03 03:01:05 +0200

    file added

--- launchers/score_babytrain.sh
+++ launchers/score_babytrain.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=47:58:58
 
 # load conda environment
 source /shared/apps/anaconda3/etc/profile.d/conda.sh
-conda activate pyannote
+conda activate /scratch2/sdas/conda/envs/pyannote
 
 
-python main.py runs/train-superdataset-test-4/ score \
+python main.py runs/train-superdataset-test-3-1/ score \
 -p X.SpeakerDiarization.BBT2 \
---model_path runs/train-superdataset-test-4/checkpoints/last.ckpt \
---classes dataset \
+--model_path runs/train-superdataset-test-3-1/checkpoints/epoch=30-MultiLabelSegmentation-XSpeakerDiarizationDATASET-MultilabelAUROC=0.773830.ckpt \
+--classes babytrain \
 --metric fscore \
---apply_folder runs/train-superdataset-test-4/apply/ \
---report_path runs/train-superdataset-test-4/results/fscore.csv
+--apply_folder runs/train-superdataset-test-3-1/apply/ \
+--report_path runs/train-superdataset-test-3-1/results/fscore.csv
